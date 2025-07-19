8d28f5bd151e3d6f0108845f4af8a8495638dce0
Jayaram <jayaramg401@gmail.com>
2025-07-19 12:41:59 +0530

    Day2 added

---
+++ Day2/findMin.java
@@ -0,0 +1,25 @@
+package Day2;
+class findMin {
+    public static void main(String[] args) {
+        int a = 34 ,b = 23,c = 12;
+        int min;
+
+        if(a<b){
+            if(a<c){
+                min = a; 
+            }
+            else{
+                min = c;
+            }
+        }
+        else{
+            if(b < c){
+                min = b;
+            }
+            else{
+                min = c;
+            }
+        }
+        System.out.println(min);
+    }
+}
